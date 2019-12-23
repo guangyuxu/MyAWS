4c4f09d3b77ce0a5c6a4b36a2ba3ca02adc20691
Gavin Xu <guangyuxu@gmail.com>
2019-12-23 23:26:46 +0800

    [GX-001] Initial spring boot project

---
+++ src/main/java/com/gavin/aws/MyAwsApplication.java
@@ -0,0 +1,13 @@
+package com.gavin.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAwsApplication.class, args);
+	}
+
+}
